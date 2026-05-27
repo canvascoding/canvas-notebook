f6173b61ca82fe1230d0320139c1d90984fd0e50
Frank Alexander Weber <190745803+canvascoding@users.noreply.github.com>
2026-05-27 16:16:39 +0200

    Use API host for license default

--- app/lib/license/instance.ts
+++ app/lib/license/instance.ts
@@ -4,7 +4,7 @@ import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 import crypto from 'crypto';
 
-const DEFAULT_LICENSE_CONTROL_PLANE_URL = 'https://notebook.canvas.holdings';
+const DEFAULT_LICENSE_CONTROL_PLANE_URL = 'https://api.canvas.holdings';
 
 function getDataDir(): string {
   return process.env.DATA || path.resolve(process.cwd(), 'data');
